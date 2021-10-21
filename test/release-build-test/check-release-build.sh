b965a9371e695707ff8ef2697a1b465fb8b376d6
Karuppiah Natarajan <karuppiahn@vmware.com>
2021-10-21 15:16:06 +0530

    check count of security popups and fail the test
    Signed-off-by: Karuppiah Natarajan <karuppiahn@vmware.com>

--- test/release-build-test/check-release-build.sh
+++ test/release-build-test/check-release-build.sh
@@ -15,6 +15,7 @@ TCE_REPO_PATH="${MY_DIR}"/../..
 
 TCE_DARWIN_TAR_BALL="${TCE_REPO_PATH}/tce-darwin-amd64-${version}.tar.gz"
 TCE_DARWIN_INSTALLATION_DIR="${TCE_REPO_PATH}/tce-darwin-amd64-${version}"
+SECURITY_ISSUE_POPUP_COUNT_FILE="${TCE_REPO_PATH}/tce-security-issue-popup-count"
 
 # "${TCE_REPO_PATH}"/hack/get-tce-release.sh  darwin
 
@@ -34,31 +35,118 @@ popd
 
 tar xvzf "${TCE_DARWIN_TAR_BALL}"
 
-osascript "${TCE_REPO_PATH}"/test/release-build-test/close-security-issue-popups.applescript &
-applescript_pid=$!
+function run_script_to_close_security_issue_popups() {
+    osascript "${TCE_REPO_PATH}"/test/release-build-test/close-security-issue-popups.applescript ${SECURITY_ISSUE_POPUP_COUNT_FILE} &
+    applescript_pid=$!
+    return $applescript_pid
+}
 
-trap '{ kill $applescript_pid ; }' EXIT
+function check_security_issue_popups() {
+    if [ ! -f ${SECURITY_ISSUE_POPUP_COUNT_FILE} ]; then
+        return 0
+    fi
+
+    SECURITY_ISSUE_POPUP_COUNT=$(cat ${SECURITY_ISSUE_POPUP_COUNT_FILE})
+
+    if [ ${SECURITY_ISSUE_POPUP_COUNT} != 0 ]; then
+        echo "There were ${SECURITY_ISSUE_POPUP_COUNT} security issue popups"
+        return 1
+    fi
+}
+
+applescript_pid=$(run_script_to_close_security_issue_popups)
 
 "${TCE_DARWIN_INSTALLATION_DIR}"/install.sh
 
+kill -9 $applescript_pid
+
+check_security_issue_popups
+
+
+applescript_pid=$(run_script_to_close_security_issue_popups)
+
 tanzu version
+
+kill -9 $applescript_pid
+
+check_security_issue_popups
+
+applescript_pid=$(run_script_to_close_security_issue_popups)
 
 tanzu cluster version
 
+kill -9 $applescript_pid
+
+check_security_issue_popups
+
+applescript_pid=$(run_script_to_close_security_issue_popups)
+
 tanzu conformance version
+
+kill -9 $applescript_pid
+
+check_security_issue_popups
+
+applescript_pid=$(run_script_to_close_security_issue_popups)
 
 tanzu diagnostics version
 
+kill -9 $applescript_pid
+
+check_security_issue_popups
+
+applescript_pid=$(run_script_to_close_security_issue_popups)
+
 tanzu kubernetes-release version
+
+kill -9 $applescript_pid
+
+check_security_issue_popups
+
+applescript_pid=$(run_script_to_close_security_issue_popups)
 
 tanzu management-cluster version
 
+kill -9 $applescript_pid
+
+check_security_issue_popups
+
+applescript_pid=$(run_script_to_close_security_issue_popups)
+
 tanzu package version
+
+kill -9 $applescript_pid
+
+check_security_issue_popups
+
+applescript_pid=$(run_script_to_close_security_issue_popups)
 
 tanzu standalone-cluster version
 
+kill -9 $applescript_pid
+
+check_security_issue_popups
+
+applescript_pid=$(run_script_to_close_security_issue_popups)
+
 tanzu pinniped-auth version
+
+kill -9 $applescript_pid
+
+check_security_issue_popups
+
+applescript_pid=$(run_script_to_close_security_issue_popups)
 
 tanzu builder version
 
+kill -9 $applescript_pid
+
+check_security_issue_popups
+
+applescript_pid=$(run_script_to_close_security_issue_popups)
+
 tanzu login version
+
+kill -9 $applescript_pid
+
+check_security_issue_popups
--- test/release-build-test/close-security-issue-popups.applescript
+++ test/release-build-test/close-security-issue-popups.applescript
@@ -1,10 +1,20 @@
-repeat
-	if application "CoreServicesUIAgent" is running then
-		tell application "System Events" to tell process "CoreServicesUIAgent"
-			if exists (button "OK" of window 1) then
-				click (button "OK" of window 1)
-			end if
-		end tell
+on run argv
+	set x to 0
+	set argv_count to count of argv
+	if argv_count is not equal to 1 then
+		return "Usage: osascript close-security-issue-popups.applescript <popup-count-data-file-path>. \nPass exactly one file path argument to the apple script"
 	end if
-	delay 1
-end repeat
+	set count_file to item 1 of argv
+	repeat
+		if application "CoreServicesUIAgent" is running then
+			tell application "System Events" to tell process "CoreServicesUIAgent"
+				if exists (button "OK" of window 1) then
+					set x to x + 1
+					do shell script "echo " & x & " > " & count_file
+					click (button "OK" of window 1)
+				end if
+			end tell
+		end if
+		delay 1
+	end repeat
+end run
